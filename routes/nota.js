7f1a790dd80a3761f0f972d0dd0b3d727d0ba739
Jahiker <rojasjahiker@gmail.com>
2019-12-04 13:09:11 -0400

    API REST Completa

--- routes/nota.js
+++ routes/nota.js
@@ -22,13 +22,15 @@ router.post('/nueva-nota', async(req, res) => {
 
 // Get con parametros
 router.get('/nota/:id', async(req, res) => {
+
     const _id = req.params.id;
+
     try {
         const notaDB = await Nota.findOne({ _id });
         res.status(200).json(notaDB);
     } catch (error) {
         return res.status(400).jason({
-            mensaje: 'Ocurrio un error',
+            mensaje: 'No se encontro la Nota',
             error
         });
     }
@@ -48,7 +50,48 @@ router.get('/nota', async(req, res) => {
             error
         });
     }
-})
+});
+
+// Eliminar una nota
+router.delete('/nota/:id', async(req, res) => {
+    const _id = req.params.id;
+
+    try {
+
+        const notaDB = await Nota.findByIdAndDelete({ _id });
+
+        if (!notaDB) {
+            return res.status(400).json({
+                mensaje: 'No se econtro la nota',
+                error
+            });
+        }
+
+        res.status(200).json(notaDB);
+
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Error al eliminar la nota',
+            error
+        });
+    }
+});
+
+// Modificar una nota
+router.put('/nota/:id', async(req, res) => {
+    const _id = req.params.id;
+    const body = req.body;
+
+    try {
+        const notaDB = await Nota.findByIdAndUpdate(_id, body, { new: true });
+        res.status(200).json(notaDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Error al modificar la nota',
+            error
+        });
+    }
+});
 
 // Exportacion de router
 module.exports = router;
